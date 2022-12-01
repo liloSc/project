9099a6caf9d072757f152544aa61be8fe784788a
liloSc <52287760+liloSc@users.noreply.github.com>
2022-12-01 20:27:33 +0100

    Display Long and Lat

--- views/location.js
+++ views/location.js
@@ -1,12 +1,17 @@
-const findLocation =() =>{
-    const status =document.querySelector('.status');
-    const success =(position) => {
+const findLocation = () => {
+    const status = document.querySelector('.status');
+    const success = (position) => {
         console.log(position)
-      //  status.textContent=position
+        // status.textContent=position
+        const crd = position.coords;
+
+        console.log('Your current position is:');
+        console.log(`Latitude : ${crd.latitude}`);
+        status.textContent = "Latitude: " + crd.latitude + " Longitude: " + crd.longitude
     }
-    const error =() => {
-      status.textContent="Unable to retrieve your location"
+    const error = () => {
+        status.textContent = "Unable to retrieve your location"
     }
-    navigator.geolocation.getCurrentPosition(success,error);
+    navigator.geolocation.getCurrentPosition(success, error);
 }
 document.querySelector('.find-state').addEventListener('click', findLocation);
