417f509bb7cb836f19640f94f6fb6c153be972cf
liloSc <52287760+liloSc@users.noreply.github.com>
2022-12-02 17:25:21 +0100

    Check if point next to cite

--- views/location.js
+++ views/location.js
@@ -15,3 +15,29 @@ const findLocation = () => {
     navigator.geolocation.getCurrentPosition(success, error);
 }
 document.querySelector('.find-state').addEventListener('click', findLocation);
+
+//48.819185 | Longitude : 2.336768
+
+const isNext = () => {
+    var coordinateCite=[48.819185,2.336768]
+    const status = document.querySelector('.place-status');
+    const success = (position) => {
+        console.log(position)
+        // status.textContent=position
+        const crd = position.coords;
+        console.log(`Latitude : ${crd.latitude}`);
+        if ((crd.latitude < (coordinateCite[0] + 0.01) && crd.latitude > (coordinateCite[0]  - 0.01))&&(crd.longitude < (coordinateCite[1]  + 0.01) && crd.longitude > (coordinateCite[1]  - 0.01))){
+      
+      //  if ((crd.latitude < (48.819185 + 0.01) && crd.latitude > (48.819185 - 0.01))&&(crd.longitude < (2.336768 + 0.01) && crd.longitude > (2.336768 - 0.01))){
+            status.textContent = "Next to CITE "+crd.latitude + " "+coordinateCite[0]+ crd.longitude+" "+coordinateCite[1]
+        }else{
+            status.textContent = "NOT !!! Next to CITE"
+        }
+           
+    }
+    const error = () => {
+        status.textContent = "Unable to retrieve your location"
+    }
+    navigator.geolocation.getCurrentPosition(success, error);
+}
+document.querySelector('.find-street').addEventListener('click', isNext);
