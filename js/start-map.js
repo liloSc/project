e0ec4ea7603684cb681df35e8397505017fce58e
liloSc <52287760+liloSc@users.noreply.github.com>
2022-12-10 18:19:08 +0100

    Cleaning

--- js/start-map.js
+++ js/start-map.js
@@ -1,5 +1,5 @@
 
-//var map = L.map('map2').setView([48.819185,2.336768], 13);
+map-home
 let map = L.map("tracker").setView([48.819185,2.336768], 13);
 
 var marker = L.marker([48.819185,2.336768]).addTo(map);
