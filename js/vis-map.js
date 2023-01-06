3beaa58edd6238a4789ae1daa1cc6ab2efa18e53
liloSc <52287760+liloSc@users.noreply.github.com>
2023-01-06 14:56:55 +0100

    Attractions Belgium

--- js/vis-map.js
+++ js/vis-map.js
@@ -23,34 +23,24 @@ var polygon = L.polygon([
     [51.51, -0.047]
 ]).addTo(map);
 
-// create a red polyline from an array of LatLng points
-var latlngs = [
-    [45.51, -122.68],
-    [37.77, -122.43],
-    [34.04, -118.2]
-];
-
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const city = urlParams.get('city');
-console.log("This is the current place" + city)
-
+//console.log("This is the current place" + city)
 $.getJSON('/get-path?city=' + city, function (data) {
-    var polyline = L.polyline(data.path, { color: 'red' }).addTo(map);
+    var polyline = L.polyline(data.path, { color: 'blue' }).addTo(map);
 
     // zoom the map to the polyline
     map.fitBounds(polyline.getBounds());
-    /*  var polygon = L.polygon([
-          data.path
-      ]).addTo(map);
-      map.fitBounds(polygon.getBounds());*/
-  //  console.log(data.path);
+
     var array1 = data.path;
-    var marker = L.marker([array1[0], array1[1]]).addTo(map);
-    //  var marker = L.marker  ([ 48.85851125395903, 2.2945349423630814 ]).addTo(map);
-    /* data.forEach(d => {
-         console.log(d);
-         var marker = L.marker(d[0],d[1]).addTo(map);
- 
-     })*/
+    var index = 0;
+    for (datapoint of data.path) {
+
+     //   var marker =
+         L.marker([datapoint[0],
+        array1[index][1]]).addTo(map);
+        index++
+    }
+
 })
